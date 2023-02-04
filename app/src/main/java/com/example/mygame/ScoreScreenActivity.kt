8593c58ffeac72300363db4533435c36f93af706
alp cetinkus <alpcetinkus@gmail.com>
2023-02-04 14:37:06 +0300

    add sharedPreferences for high score

--- app/src/main/java/com/example/mygame/ScoreScreenActivity.kt
+++ app/src/main/java/com/example/mygame/ScoreScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mygame
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,21 @@ class ScoreScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score_screen)
+
+        val score = intent.getIntExtra("score", 0)
+        score_end.text = score.toString()
+
+        val sp = getSharedPreferences("result", Context.MODE_PRIVATE)
+        val highScore = sp.getInt("highScore",0)
+
+        if (score > highScore) {
+            val editor = sp.edit()
+            editor.putInt("highScore",score)
+            editor.commit()
+            highScore_end.text = score.toString()
+        } else {
+            highScore_end.text = highScore.toString()
+        }
 
         again_game_btn.setOnClickListener {
             startActivity(Intent(this@ScoreScreenActivity,MainActivity::class.java))
